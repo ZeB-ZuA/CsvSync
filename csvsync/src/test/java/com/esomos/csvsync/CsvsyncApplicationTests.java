156396293ff7e54ff947608ccb0db7ac0b01b88a
SUA GARCIA JHOAN SEBASTIAN <sebastiansua90@gmail.com>
2024-10-08 18:07:45 -0500

    Proyect inti

---
+++ csvsync/src/test/java/com/esomos/csvsync/CsvsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esomos.csvsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
