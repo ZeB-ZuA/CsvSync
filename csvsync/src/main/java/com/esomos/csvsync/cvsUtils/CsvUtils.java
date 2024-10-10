3a5e9aa45a74e5713159614484e42604e4720551
SUA GARCIA JHOAN SEBASTIAN <sebastiansua90@gmail.com>
2024-10-10 15:14:44 -0500

    SQL insertion completed, all attributes were inserted in VARCHAR format.

--- csvsync/src/main/java/com/esomos/csvsync/cvsUtils/CsvUtils.java
+++ csvsync/src/main/java/com/esomos/csvsync/cvsUtils/CsvUtils.java
@@ -1,5 +1,96 @@
 package com.esomos.csvsync.cvsUtils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+import com.opencsv.*;
+
 public class CsvUtils {
+
+    public String inferDataType(String value) {
+        if (isBoolean(value)) {
+            return "BOOLEAN";
+        } else if (isInteger(value)) {
+            return "INT";
+        } else if (isDouble(value)) {
+            return "DOUBLE PRECISION";
+        } else if (isTimestamp(value)) { 
+            return "TIMESTAMP";
+        } else if (isDate(value)) {
+            return "DATE";
+        } else if (isTime(value)) {
+            return "TIME";
+        } else {
+            return "TEXT"; 
+        }
+    }
+
+    private static boolean isBoolean(String value) {
+        return value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false");
+    }
+
+    private static boolean isInteger(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isDouble(String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isDate(String value) {
+        return isValidFormat("yyyy-MM-dd", value) || isValidFormat("MM/dd/yyyy", value);
+    }
     
+    private static boolean isTime(String value) {
+        return isValidFormat("HH:mm:ss", value) || isValidFormat("hh:mm:ss a", value); 
+    }
+    
+    
+    
+    private static boolean isTimestamp(String value) {
+        return isValidFormat("yyyy-MM-dd HH:mm:ss.SSS", value) || isValidFormat("yyyy-MM-dd HH:mm:ss", value);
+    }
+    
+    private static boolean isValidFormat(String format, String value) {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        sdf.setLenient(false);
+        try {
+            sdf.parse(value);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+
+    
+    public String cleanColumnName(String columnName) {
+        return columnName.trim().replaceAll(" ", "_").replaceAll("[^a-zA-Z0-9_]", "");
+    }
+    
+
+    public static String[] readSampleRow(String filePath) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String firstLine = br.readLine();
+            if (firstLine != null && firstLine.startsWith("\uFEFF")) {
+                firstLine = firstLine.substring(1);
+            }
+    
+            return firstLine.split(";");
+        }
+    }
 }
+
--- csvsync/src/main/java/com/esomos/csvsync/service/CreateTableService.java
+++ csvsync/src/main/java/com/esomos/csvsync/service/CreateTableService.java
@@ -3,53 +3,66 @@ package com.esomos.csvsync.service;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import com.esomos.csvsync.cvsUtils.CsvUtils;
 
 @Service
 public class CreateTableService {
 
     private final JdbcTemplate jdbcTemplate;
+    private final CsvUtils csvUtils = new CsvUtils();
 
     public CreateTableService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+
     }
 
     public void createTable(String[] headers, String filePath) {
-        StringBuilder sqlTable = new StringBuilder("CREATE TABLE IF NOT EXISTS ");
-        sqlTable.append(obtainTableName(filePath)).append(" (");
+        String schemaName = obtainTableName(filePath);
+        StringBuilder sqlSchema = new StringBuilder("CREATE SCHEMA IF NOT EXISTS ");
+        sqlSchema.append(schemaName).append(";");
 
-        for (String header : headers) {
-            sqlTable.append(header).append(" VARCHAR(255), ");
-        }
-        sqlTable.setLength(sqlTable.length() - 2);
-        sqlTable.append(");");
+        StringBuilder sqlTable = new StringBuilder("CREATE TABLE IF NOT EXISTS ");
+        sqlTable.append(schemaName).append(".").append(schemaName).append(" (");
 
         try {
+
+            for (String header : headers) {
+                String cleanedHeader = csvUtils.cleanColumnName(header);
+                sqlTable.append(cleanedHeader).append(" VARCHAR(255), ");
+            }
+            sqlTable.setLength(sqlTable.length() - 2);
+            sqlTable.append(");");
+
+            jdbcTemplate.execute(sqlSchema.toString());
+            System.out.println("Schema created successfully: " + schemaName);
             jdbcTemplate.execute(sqlTable.toString());
-            System.out.println("Table created successfully: " + obtainTableName(filePath));
+            System.out.println("Table created successfully: " + schemaName + "." + schemaName);
         } catch (Exception e) {
-            System.err.println("Error creating table: " + e.getMessage());
+            System.err.println("Error creating schema or table (Exception): " + e.getMessage());
         }
     }
 
     public void insertData(String[] headers, String[] data, String filePath) {
+        String schemaName = obtainTableName(filePath);
         StringBuilder sqlInsert = new StringBuilder("INSERT INTO ");
-        sqlInsert.append(obtainTableName(filePath)).append(" (");
+        sqlInsert.append(schemaName).append(".").append(schemaName).append(" (");
 
         for (String header : headers) {
-            sqlInsert.append(header).append(", ");
+            String cleanHeader = csvUtils.cleanColumnName(header);
+            sqlInsert.append(cleanHeader).append(", ");
         }
-        sqlInsert.setLength(sqlInsert.length() - 2); // Elimina la última coma
+        sqlInsert.setLength(sqlInsert.length() - 2);
         sqlInsert.append(") VALUES (");
 
         for (String value : data) {
-            sqlInsert.append("'").append(value).append("', ");
+            sqlInsert.append("'").append(value.replace("'", "''")).append("', ");
         }
-        sqlInsert.setLength(sqlInsert.length() - 2); // Elimina la última coma
+        sqlInsert.setLength(sqlInsert.length() - 2);
         sqlInsert.append(");");
 
         try {
             jdbcTemplate.update(sqlInsert.toString());
-            System.out.println("Data inserted successfully into: " + obtainTableName(filePath));
+            System.out.println("Data inserted successfully into: " + schemaName + "." + schemaName);
         } catch (Exception e) {
             System.err.println("Error inserting data: " + e.getMessage());
         }
